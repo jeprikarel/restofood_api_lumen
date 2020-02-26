beaeb636659156de23a3119466de22be3a123000
jeprikarel <jeprikarel@gmail.com>
2020-02-26 11:03:31 +0700

    update endpoint.dart

--- lib/core/config/endpoint.dart
+++ lib/core/config/endpoint.dart
@@ -1,5 +1,11 @@
 
 class Endpoint {
-  static String _baseURL = "https://leeyurani.com/restofood/public/api";
+  //static String _baseURL = "https://leeyurani.com/restofood/public/api";
+  static String _baseURL = "http://localhost/restofood20200229/public/api";
   static String baseFood = "${_baseURL}/foods";
+
+  static String baseCity = "${_baseURL}/city";
+
+  //Auth routes
+  static String register = "${_baseURL}/auth/register";
 }
